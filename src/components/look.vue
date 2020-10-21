761f417dea1e9cc20450bb986470ead1f845dadd
WaNgLu001 <lg1065849.com>
2020-10-21 16:14:21 +0800

    完成

--- src/components/look.vue
+++ src/components/look.vue
@@ -8,44 +8,44 @@
     height = "600"
     style="width: 100%">
     <el-table-column
-      prop="username"
+      prop="NAME"
       label="姓名"
       width="80">
     </el-table-column>
     <el-table-column
       prop="mon"
       label="周一"
-      width="50">
+      width="60">
     </el-table-column>
     <el-table-column
       prop="tues"
       label="周二"
-      width="50">
+      width="60">
     </el-table-column>
     <el-table-column
       prop="wed"
       label="周三"
-      width="50">
+      width="60">
     </el-table-column>
     <el-table-column
       prop="thur"
       label="周四"
-      width="50">
+      width="60">
     </el-table-column>
     <el-table-column
       prop="fri"
       label="周五"
-      width="50">
+      width="60">
     </el-table-column>
     <el-table-column
       prop="sat"
       label="周六"
-      width="50">
+      width="60">
     </el-table-column>
     <el-table-column
       prop="sun"
       label="周日"
-      width="50">
+      width="60">
     </el-table-column>
 
   </el-table></div>
@@ -58,7 +58,7 @@ export default {
         }
     },
     async created(){
-         const { data } = await this.$axios.get(`/getAllUserTimer?type="2"`);
+         const { data } = await this.$axios.get(`/getQRClass`);
          this.tableData = data.data
     }
 }
